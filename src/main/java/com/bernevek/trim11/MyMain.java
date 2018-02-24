46093a7fafdf5d648c9f8af8a5474e7cc2bc9e68
bernevek <bernevek1995@ukr.net>
2018-02-24 13:41:34 +0200

    created new maven project

---
+++ src/main/java/com/bernevek/trim11/MyMain.java
@@ -0,0 +1,13 @@
+package com.bernevek.trim11;
+
+/**
+ * Hello world!
+ *@author ivan
+ */
+public class MyMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
